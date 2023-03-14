b11b0aed2e381a8c5265c01194c047c966195097
Tudor Helesteanu <tudor.helesteanu@gmail.com>
2023-03-14 17:14:19 +0200

    Initial commit

---
+++ src/main/java/com/softvision/springBootGraphQLStarterApp/SpringBootGraphQlStarterAppApplication.java
@@ -0,0 +1,13 @@
+package com.softvision.springBootGraphQLStarterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQlStarterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphQlStarterAppApplication.class, args);
+	}
+
+}
